7eecd4ed0ca78375352adb617494d67f7b37abb5
Mubtasim Fuad <68302816+rafisics@users.noreply.github.com>
2024-04-07 12:25:40 +0600

    Remove ltex-ls

--- nvim.chad/lua/plugins/init.lua
+++ nvim.chad/lua/plugins/init.lua
@@ -22,7 +22,8 @@ return {
   		ensure_installed = {
   			"lua-language-server", "stylua",
   			--"html-lsp", "css-lsp" , "prettier"
-        "texlab", "ltex-ls", "bibtex-tidy" 
+        "texlab", --"ltex-ls", 
+        "bibtex-tidy" 
   		},
   	},
   },
