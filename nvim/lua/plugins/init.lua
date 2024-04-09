5d760d6a159058c7cf8fd169b2c22ffd3ca19402
Mubtasim Fuad <68302816+rafisics@users.noreply.github.com>
2024-04-09 19:29:44 +0600

    Update conform.nvim

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1,10 +1,10 @@
 return {
   {
     "stevearc/conform.nvim",
-    event = 'BufWritePre', -- uncomment for format on save
-    config = function()
-      require "configs.conform"
-    end,
+    -- event = 'BufWritePre', -- uncomment for format on save
+    -- config = function()
+    --   require "configs.conform"
+    -- end,
   },
 
   -- These are some examples, uncomment them if you want to see them work!
