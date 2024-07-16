775282bce98945442161a08af4d67fd06820f1c4
Mubtasim Fuad <68302816+rafisics@users.noreply.github.com>
2024-07-16 09:10:51 +0600

    Update

--- nvim/lua/plugins/which-key.lua
+++ nvim/lua/plugins/which-key.lua
@@ -1,5 +1,6 @@
 return {
   "folke/which-key.nvim",
+  dependencies = { 'echasnovski/mini.icons' },
   event = "VeryLazy",
   init = function()
     vim.o.timeout = true
@@ -27,33 +28,52 @@ return {
           -- No actual key bindings are created
         },
       },
-      key_labels = {
-        -- override the label used to display some keys. It doesn't effect WK in any other way.
-        -- For example:
-        -- ["<space>"] = "SPC",
-        -- ["<CR>"] = "RET",
-        -- ["<tab>"] = "TAB",
-      },
+      -- key_labels = {
+      --   -- override the label used to display some keys. It doesn't effect WK in any other way.
+      --   -- For example:
+      --   -- ["<space>"] = "SPC",
+      --   -- ["<CR>"] = "RET",
+      --   -- ["<tab>"] = "TAB",
+      -- },
       -- triggers = "auto", -- automatically setup triggers
       triggers = { "<leader>" }, -- or specify a list manually
       -- add operators that will trigger motion and text object completion
       -- to enable native operators, set the preset / operators plugin above
       -- operators = { gc = "Comments" },
+      replace = {
+        key = {
+          function(key)
+            return require("which-key.view").format(key)
+          end,
+          -- { "<Space>", "SPC" },
+        },
+        -- desc = {
+        --   { "<Plug>%((.*)%)", "%1" },
+        --   { "^%+", "" },
+        --   { "<[cC]md>", "" },
+        --   { "<[cC][rR]>", "" },
+        --   { "<[sS]ilent>", "" },
+        --   { "^lua%s+", "" },
+        --   { "^call%s+", "" },
+        --   { "^:%s*", "" },
+        -- },
+      },
       icons = {
         breadcrumb = "»", -- symbol used in the command line area that shows your active key combo
         separator = "➜", -- symbol used between a key and it's label
         group = "+",      -- symbol prepended to a group
       },
-      popup_mappings = {
+      keys = {
         scroll_down = "<c-d>", -- binding to scroll down inside the popup
         scroll_up = "<c-u>",   -- binding to scroll up inside the popup
       },
-      window = {
+      win = {
         border = "rounded",       -- none, single, double, shadow
-        position = "bottom",      -- bottom, top
-        margin = { 1, 0, 1, 0 },  -- extra window margin [top, right, bottom, left]
+        -- position = "bottom",      -- bottom, top
+        -- margin = { 1, 0, 1, 0 },  -- extra window margin [top, right, bottom, left]
         padding = { 2, 2, 2, 2 }, -- extra window padding [top, right, bottom, left]
-        winblend = 0,
+        -- title = true,
+        -- title_pos = "center",
         zindex = 1000,            -- positive value to position WhichKey above other floating windows.
       },
       layout = {
@@ -62,17 +82,17 @@ return {
         spacing = 3,                                                                -- spacing between columns
         align = "left",                                                             -- align columns left, center or right
       },
-      ignore_missing = true,                                                        -- enable this to hide mappings for which you didn't specify a label
-      hidden = { "<silent>", "<cmd>", "<Cmd>", "<CR>", "call", "lua", "^:", "^ " }, -- hide mapping boilerplate
-      triggers_nowait = {
-      },
-      triggers_blacklist = {
-        -- list of mode / prefixes that should never be hooked by WhichKey
-        -- this is mostly relevant for key maps that start with a native binding
-        -- most people should not need to change this
-        i = { "j", "k" },
-        v = { "j", "k" },
-      },
+      -- ignore_missing = true,                                                        -- enable this to hide mappings for which you didn't specify a label
+      -- hidden = { "<silent>", "<cmd>", "<Cmd>", "<CR>", "call", "lua", "^:", "^ " }, -- hide mapping boilerplate
+      -- triggers_nowait = {
+      -- },
+      -- triggers_blacklist = {
+      --   -- list of mode / prefixes that should never be hooked by WhichKey
+      --   -- this is mostly relevant for key maps that start with a native binding
+      --   -- most people should not need to change this
+      --   i = { "j", "k" },
+      --   v = { "j", "k" },
+      -- },
       -- disable the WhichKey popup for certain buf types and file types.
       -- Disabled by default for Telescope
       disable = {
