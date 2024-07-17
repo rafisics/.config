2ed4953ff56b271d563fde8e2d4267d588e3b72e
Mubtasim Fuad <68302816+rafisics@users.noreply.github.com>
2024-07-17 06:51:20 +0600

    Update

--- nvim/lua/plugins/which-key.lua
+++ nvim/lua/plugins/which-key.lua
@@ -1,6 +1,9 @@
 return {
   "folke/which-key.nvim",
-  dependencies = { 'echasnovski/mini.icons' },
+  dependencies = {
+    "echasnovski/mini.icons",
+    "nvim-tree/nvim-web-devicons",
+  },
   event = "VeryLazy",
   init = function()
     vim.o.timeout = true
