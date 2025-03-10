c948aef7e11a9db0dbcd6f2af7edce7c03a119b4
Mubtasim Fuad <68302816+rafisics@users.noreply.github.com>
2025-03-10 19:33:00 +0600

    Update

--- fish/config.fish
+++ fish/config.fish
@@ -1,16 +1,17 @@
+# Ensure Fish is in interactive mode before running commands
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+# Remove the mapping <C-t> which is used to close the terminal in NeoVim
 bind --erase \ct
-# removes the mapping <C-t> which is being used to close the terminal in NeoVim
 
+# Zoxide
 if type -q zoxide
-zoxide init fish --cmd cd | source
-# removes the mapping <C-t> which is being used to close the terminal in NeoVim
+    zoxide init fish --cmd cd | source
 end
 
-# enables starship config 
+# Starship
 starship init fish | source
 
 # Nvim
@@ -19,7 +20,7 @@ source ~/.config/nvim.fish
 # Astrophysics
 source ~/github/my-scripts/astro/astro.fish
 
-# Python 
+# Python
 function activate-py-nvim-env
     source ~/.venvs/nvim-env/bin/activate.fish
 end
@@ -32,10 +33,16 @@ end
 export EDITOR="nvim"
 
 function y
-	set tmp (mktemp -t "yazi-cwd.XXXXXX")
-	yazi $argv --cwd-file="$tmp"
-	if set cwd (command cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
-		builtin cd -- "$cwd"
-	end
-	rm -f -- "$tmp"
+    set tmp (mktemp -t "yazi-cwd.XXXXXX")
+    yazi $argv --cwd-file="$tmp"
+    if set cwd (command cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+        builtin cd -- "$cwd"
+    end
+    rm -f -- "$tmp"
+end
+
+# Bundler
+if type -q bundle
+    set -x GEM_HOME /home/rafi/gems
+    set -x PATH $GEM_HOME/bin $PATH
 end
