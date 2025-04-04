dcfd845c44c5a822ff2423a0ff8c28ad5e817e87
Mubtasim Fuad <68302816+rafisics@users.noreply.github.com>
2025-04-04 18:15:12 +0600

    Remove previous switch method

--- nvim.fish
+++ nvim.fish
@@ -64,52 +64,7 @@ function delete_nvim_local_cache
     rm -rf ~/.cache/nvim
 end
 
-# Commands to switch from NeoTeX to NvChad
-function switch_to_nvchad
-    # Back up NeoTeX
-    mv ~/.config/nvim{,.bak}
-    mv ~/.local/share/nvim{,.bak}
-    mv ~/.local/state/nvim{,.bak}
-    mv ~/.cache/nvim{,.bak}
-
-    # Move to NvChad
-    mv ~/.config/nvim{.chad,}
-    mv ~/.local/share/nvim{.chad,}
-    mv ~/.local/state/nvim{.chad,}
-    mv ~/.cache/nvim{.chad,}
-end
-
-# Commmands to switch from NvChad to NeoTeX
-function switch_to_neotex
-    # Back up NvChad
-    mv ~/.config/nvim{,.chad}
-    mv ~/.local/share/nvim{,.chad}
-    mv ~/.local/state/nvim{,.chad}
-    mv ~/.cache/nvim{,.chad}
-
-    # Move to NeoTeX
-    mv ~/.config/nvim{.bak,}
-    mv ~/.local/share/nvim{.bak,}
-    mv ~/.local/state/nvim{.bak,}
-    mv ~/.cache/nvim{.bak,}
-end
-
-# Command to toggle between NeoTeX and NvChad
-function toggle_nvim
-    if test -e ~/.config/nvim.chad -a ! -e ~/.config/nvim.bak
-        # Switch from NeoTeX to NvChad
-        switch_to_nvchad
-    else if test -e ~/.config/nvim.bak -a ! -e ~/.config/nvim.chad
-        # Switch from NvChad to NeoTeX
-        switch_to_neotex
-    else
-        echo "Error: Unable to determine current nvim configuration."
-        return 1
-    end
-    nvim
-end
-
-# Better Method for Neovim config switch - https://youtu.be/LkHjJlSgKZY - https://gist.github.com/elijahmanor/b279553c0132bfad7eae23e34ceb593b?permalink_comment_id=5312460#gistcomment-5312460
+# Neovim config switch - https://youtu.be/LkHjJlSgKZY - https://gist.github.com/elijahmanor/b279553c0132bfad7eae23e34ceb593b?permalink_comment_id=5312460#gistcomment-5312460
 
 function nvim-tex
     env NVIM_APPNAME=nvim.bak nvim
