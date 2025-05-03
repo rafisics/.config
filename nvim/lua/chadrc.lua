7ce6c5ed0252df922e3ee2a1917ad6c7f1f9870d
Mubtasim Fuad <68302816+rafisics@users.noreply.github.com>
2025-05-03 14:14:49 +0600

    Update theme

--- nvim/lua/chadrc.lua
+++ nvim/lua/chadrc.lua
@@ -8,11 +8,18 @@ local M = {}
 M.base46 = {
   theme = "gruvbox",
   theme_toggle = { "gruvbox", "gruvbox_light" },
-  transparency = true,
   hl_override = {
     ["Visual"] = { bg = "#787859", fg = "#D4D4D4" },
-    -- ["Comment"] = { fg = "#B5B596" },
   },
 }
+
+-- Apply the same style to multiple comment groups
+local comment_groups = { "Comment", "@comment", "texComment" }
+for _, group in ipairs(comment_groups) do
+  M.base46.hl_override[group] = {
+    -- fg = "#B5B596", 
+    italic = true 
+  }
+end
 
 return M
